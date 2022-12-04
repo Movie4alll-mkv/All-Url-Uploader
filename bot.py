7aefc34afa66608b4c6951238a923e78f2b167cd
MeharFaizan1 <101054964+MeharFaizan1@users.noreply.github.com>
2022-12-04 21:05:48 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -19,7 +19,8 @@
 # LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE
-
+from aiohttp import web
+from plugins import web_server
 from pyrogram import Client, idle
 from Uploader.config import Config
 import os
