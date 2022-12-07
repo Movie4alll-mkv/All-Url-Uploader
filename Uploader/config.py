ed5d1a240e9c5a4a95c46c002e2771f770d85484
Movie4alll-mkv <77849768+Movie4alll-mkv@users.noreply.github.com>
2022-12-07 12:27:54 +0500

    Update config.py

--- Uploader/config.py
+++ Uploader/config.py
@@ -74,4 +74,4 @@ class Config(object):
     ADL_BOT_RQ = {}
     AUTH_USERS = list({int(x) for x in os.environ.get("AUTH_USERS", "0").split()})
     AUTH_USERS.append(OWNER_ID)
-    Port = "8080"
+    PORT = "0.0.0.0"
