6716a0aa87244f94d4d8a476bc49a43de5a92612
Movie4alll-mkv <77849768+Movie4alll-mkv@users.noreply.github.com>
2022-12-07 11:50:37 +0500

    Update config.py

--- Uploader/config.py
+++ Uploader/config.py
@@ -36,7 +36,7 @@ logging.basicConfig(
 
 class Config(object):
     # get a token from @BotFather
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", "5421870037:AAEBlGB3JurlEsBcka_do2tz6_7IJswdGpY")
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "5204273691:AAHcmfkQ-LZcVcWEA44drE3gn7mHSvbOEsY")
     # The Telegram API things
     API_ID = int(os.environ.get("API_ID", "6534707"))
     API_HASH = os.environ.get("API_HASH", "4bcc61d959a9f403b2f20149cbbe627a")
